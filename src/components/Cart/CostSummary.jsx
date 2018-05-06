975bdffdd47ecd8787377a6821cbd22f98b1469b
Billy Derringer <billyderringer7@gmail.com>
2018-05-06 00:40:09 -0500

    added total for all cart items

--- src/components/Cart/CostSummary.jsx
+++ src/components/Cart/CostSummary.jsx
@@ -1,32 +1,41 @@
 import React, {Component} from 'react'
 import './Cart.css'
+import {connect} from "react-redux";
 
-class CostSummary extends Component{
-    render() {
-        return (
-            <div className="row date-summary">
-                <div className="col-6 out-title">
-                    <h3>Subtotal:</h3>
-                </div>
-                <div className="col-6 current-out">
-                    <h3>$200</h3>
-                </div>
-                <div className="col-6 in-title">
-                    <h3>Tax(8%):</h3>
-                </div>
-                <div className="col-6 current-in">
-                    <h3>4.99</h3>
-                </div>
-                <hr />
-                <div className="col-6 total-row">
-                    <h3>TOTAL:</h3>
-                </div>
-                <div className="col-6 total-row">
-                    <h3>$200</h3>
-                </div>
+function CostSummary(props){
+    let subtotal = props.subtotal
+    let taxRate = .0625
+    let tax = props.subtotal * taxRate
+    let total = subtotal + tax
+    return (
+        <div className="row date-summary">
+            <div className="col-6 out-title">
+                <h3>Subtotal:</h3>
             </div>
-        )
+            <div className="col-6 current-out">
+                <h3>$ {parseFloat(subtotal).toFixed(2)}</h3>
+            </div>
+            <div className="col-6 in-title">
+                <h3>Tax({taxRate * 100}%):</h3>
+            </div>
+            <div className="col-6 current-in">
+                <h3>$ {parseFloat(tax).toFixed(2)}</h3>
+            </div>
+            <hr />
+            <div className="col-6 total-row">
+                <h3>TOTAL:</h3>
+            </div>
+            <div className="col-6 total-row">
+                <h3>$ {parseFloat(total).toFixed(2)}</h3>
+            </div>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) =>{
+    return{
+        subtotal: state.inventoryReducer.subtotal
     }
 }
 
-export default CostSummary
+export default connect(mapStateToProps)(CostSummary)
