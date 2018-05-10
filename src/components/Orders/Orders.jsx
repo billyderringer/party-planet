21ae7865a2e857cfc779730d82d531ff6e394669
Billy Derringer <billyderringer7@gmail.com>
2018-05-09 21:32:08 -0500

    removed price total in orders summary

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
-import OrderResult from './OrderResult'
+import {connect} from 'react-redux'
 import './Orders.css'
 
 class Orders extends Component {
     render() {
+        const orders = this.props.orders.map((item,i) => {
+            return <tr className="order-row"
+                       key={i} >
+                    <td className="order-name">{item.firstName} {item.lastName}</td>
+                <td className="view-col"><button className="btn view-btn">VIEW</button></td>
+                    <td className="return-col"><button className="btn return-btn">RETURN</button></td>
+                </tr>
+        })
         return (
             <div className="orders-container">
-                <h2><strong>Orders</strong></h2>
-                <table className="table">
-                    <tbody className="orders-results">
-                    <tr>
-                        <th className="name-header">Name</th>
-                        <th className="total-header">Total</th>
-                    </tr>
-                    <OrderResult />
+                <table>
+                    <tbody>
+                        {orders}
                     </tbody>
                 </table>
             </div>
@@ -21,4 +24,10 @@ class Orders extends Component {
     }
 }
 
-export default Orders
+const mapStateToProps = (state) => {
+    return{
+        orders: state.customerReducer.customers
+    }
+}
+
+export default connect (mapStateToProps)(Orders)
